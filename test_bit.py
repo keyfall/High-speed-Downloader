ffc075d8c294c8f5f8911567e3e6a89f50e12990
keyfall <2536726426@qq.com>
2024-03-11 11:46:59 +0800

    增加aria2功能

--- test_bit.py
+++ test_bit.py
@@ -1,7 +1,13 @@
 import os, subprocess
 
 magnet_link = "magnet:?xt=urn:btih:C168B7AD1413C99338EAEFA9C1A0E781F27D3072"
+trackers = []
+with open("trackers_best.txt", "r") as file:
+    for tracker in file:
+        trackers.append(tracker.strip())
+trackers_str = ",".join(trackers)
 
-command = 'ping google.com -n 3'
-output = subprocess.check_output(command, shell=True, text=True)
-print(output)
+print(trackers_str)
+# command = 'aria2c --bt-tracker=$trackers_str $magnet_link"'
+# output = subprocess.check_output(command, shell=True, text=True)
+# print(output)
